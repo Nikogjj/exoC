fd1b53a615f781eb4ce06371f4d4b9561a45851d
niko <nikolaguemmoud@hotmail.fr>
2024-09-30 11:34:35 +0200

    a

--- exercice C2.c
+++ exercice C2.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    int a=1;
+
+    int a= 1;
     int b=0;
     while (b<67)
     {
         b++;
-    int somme=a+b;
+        int somme=a+b;
         printf("%d+%d=%d\n",a,b,somme);
+        // j'ai mis le "a" après le printf (SANS METTRE "INT a" CAR SINON IL DECLARE UNE NOUVELLE VALEUR DANS LE "WHILE")
+        a =somme;
     }
     
 }
